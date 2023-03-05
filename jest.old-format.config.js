07879952b3519c05be318197fc1fc5dc7628c0c2
Igor <igor-dv@users.noreply.github.com>
2023-03-05 13:29:41 +0200

    try to fail ci

--- jest.old-format.config.js
+++ jest.old-format.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   cacheDirectory: './.jest/cache',
-  snapshotFormat: {
-    escapeString: true,
-    printBasicPrototype: true,
-  },
+  // snapshotFormat: {
+  //   escapeString: true,
+  //   printBasicPrototype: true,
+  // },
 };
